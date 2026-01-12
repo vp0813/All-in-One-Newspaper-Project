cbdff27ee8b25b903e3c50bb69de056b986e0879
vp0813 <patravaishnavi61@gmail.com>
2026-01-12 17:16:05 +0530

    Boxes Added

--- All in one Newspaper (Project)/script.js
+++ All in one Newspaper (Project)/script.js
@@ -11,3 +11,45 @@ if(webname){
         window.location.href = "main.html";
     }
 }
+
+let l = document.getElementById("light");
+if(l){
+    l.onclick = function(){
+        document.body.style.backgroundColor = "white";
+    }
+}
+
+let d = document.getElementById("dark");
+if(d){
+    d.onclick = function(){
+        document.body.style.backgroundColor = "black";
+    }
+}
+
+let saved = document.getElementById("save");
+if(saved){
+    saved.onclick = function(){
+        window.location.href = "saved.html";
+    }
+}
+
+let alertn = document.getElementById("alert-news");
+if(alertn){
+    alertn.onclick = function(){
+        window.location.href = "alertnews.html";
+    }
+}
+
+let link1 = document.getElementById("github");
+if(link1){
+    link1.onclick = function(){
+        window.location.href = "https://github.com/vp0813";
+    }
+}
+
+let link2 = document.getElementById("linkedIn");
+if(link2){
+    link2.onclick = function(){
+        window.location.href = "https://www.linkedin.com/in/vaishnavi-patra/";
+    }
+}
